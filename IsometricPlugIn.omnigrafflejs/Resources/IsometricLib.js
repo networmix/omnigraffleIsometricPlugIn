2cd3f0b58e65c55e37bda2f6d6462989ea4d2374
Andrey <networmix@gmail.com>
2021-08-15 21:47:15 +0100

    adding scale methods

--- IsometricPlugIn.omnigrafflejs/Resources/IsometricLib.js
+++ IsometricPlugIn.omnigrafflejs/Resources/IsometricLib.js
@@ -2,24 +2,34 @@ var _ = function() {
     var IsometricLib = new PlugIn.Library(new Version("0.5"));
 
     IsometricLib.skewX = function(points, deg){
-        new_points = []
+        newPoints = []
         for (let p of points) {
-            new_points.push(new Point(p.x + p.y * Math.tan(deg * Math.PI / 180), p.y))
+            newPoints.push(new Point(p.x + p.y * Math.tan(deg * Math.PI / 180), p.y))
         }
-        return new_points
+        return newPoints
     }
 
     IsometricLib.skewY = function(points, deg){
-        new_points = []
+        newPoints = []
         for (let p of points) {
-            new_points.push(new Point(p.x, p.y + p.x * Math.tan(deg * Math.PI / 180)))
+            newPoints.push(new Point(p.x, p.y + p.x * Math.tan(deg * Math.PI / 180)))
         }
-        return new_points
+        return newPoints
     }
-    
+
+    IsometricLib.scaleX = function(size, factor){
+        newSize = new Size(size.width * factor, size.height)
+        return newSize
+    }
+
+    IsometricLib.scaleY = function(size, factor){
+        newSize = new Size(size.width, size.height * factor)
+        return newSize
+    }
+
     // returns a list of functions
     IsometricLib.handlers = function(){
-        return "\n// IsometricLib ©2021 Andrey Golovanov\n• skewX(points, deg)\n• skewY(points, deg)"
+        return "\n// IsometricLib ©2021 Andrey Golovanov\n• skewX(points, deg)\n• skewY(points, deg)\n• scaleX(size, factor)\n• scaleY(size, factor)"
     }
 
     // returns contents of matching strings file
---
+++ IsometricPlugIn.omnigrafflejs/Resources/scaleVerticallyCos30.js
@@ -0,0 +1,21 @@
+var _ = function(){
+    var action = new PlugIn.Action(function(selection){
+        var isometricLib = this.IsometricLib
+        shape = selection.graphics[0]
+        if (shape.shape !== "Bezier") {
+            shape.shape = "Bezier"
+        }
+        newGeometry = shape.geometry
+        newGeometry.size = isometricLib.scaleY(newGeometry.size, Math.sqrt(3) / 2)
+        shape.geometry = newGeometry
+    });
+
+    // result determines if the action menu item is enabled
+    action.validate = function(selection){
+        // check to see if any graphics are selected
+        if (selection.graphics.length > 0){return true} else {return false}
+    };
+
+    return action;
+}();
+_;
