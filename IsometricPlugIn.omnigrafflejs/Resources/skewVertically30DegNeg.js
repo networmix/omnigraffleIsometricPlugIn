201e177799ff345f6a0d57a62b5698554c238e53
Andrey Golovanov <networmix@gmail.com>
2026-01-27 22:35:16 -0800

    fix

--- IsometricPlugIn.omnigrafflejs/Resources/skewVertically30DegNeg.js
+++ IsometricPlugIn.omnigrafflejs/Resources/skewVertically30DegNeg.js
@@ -1,7 +1,13 @@
 var _ = function() {
-    var action = this.IsometricLib.createAction(function(graphic, lib) {
-        lib.applySkewY(graphic, -30);
+    var action = new PlugIn.Action(function(selection) {
+        var lib = this.IsometricLib;
+        for (var i = 0; i < selection.graphics.length; i++) {
+            lib.applySkewY(selection.graphics[i], -30);
+        }
     });
+    action.validate = function(selection) {
+        return selection.graphics.length > 0;
+    };
     return action;
-}.call(this);
+}();
 _;
